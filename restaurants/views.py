6001fd3402e5a5683fbde153664b62d83322e192
julio <juliomenaya@gmail.com>
2022-04-19 03:23:07 -0500

    fix: setting owner at restaurant creation

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from restaurants.models import Restaurant
@@ -12,3 +13,12 @@ class ResturantsViewSet(ModelViewSet):
 
     def get_queryset(self):
         return self.queryset.filter(owner=self.request.user.owner)
+    
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        restaurant = Restaurant.objects.create(
+            owner=self.request.user.owner, name=serializer.validated_data['name'])
+        return Response({'id': restaurant.id, 'name': restaurant.name})
+        
