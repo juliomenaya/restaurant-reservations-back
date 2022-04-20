6ca196fa800e398bf85699e8d31a0522d4d3b196
julio <juliomenaya@gmail.com>
2022-04-20 16:10:38 -0500

    restaurant coupons endpoint

--- tickets/views.py
+++ tickets/views.py
@@ -1,4 +1,6 @@
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from tickets.models import Coupon
@@ -12,3 +14,11 @@ class CouponsViewSet(ModelViewSet):
 
     def get_queryset(self):
         return self.queryset.filter(restaurant__owner=self.request.user.owner)
+
+
+    @action(detail=False, methods=['get'])
+    def restaurant_coupons(self, request):
+        restaurant_id = self.request.query_params.get('restaurant')
+        qs = self.queryset.filter(restaurant_id=restaurant_id)
+        data = self.serializer_class(qs, many=True)
+        return Response(data.data)
